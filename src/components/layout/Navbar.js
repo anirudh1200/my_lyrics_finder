b5d88d58c98603927fb06bdd7ab6b96cc814a89c
= <anirudh.b.123@gmail.com>
2018-12-31 15:02:49 +0530

    added navbar and image

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = props => {
+    return(
+        <nav>
+            <div className="navbar navbar-dark bg-dark">
+                <span className="navbar-brand mb-0 h1 mx">My Music</span>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
